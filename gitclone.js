cf12b2d684d5b31373684605b6edf6bde62253c3
xuting <xutingsa@163.com>
2026-01-06 12:27:23 +0800

    git拉取和终端美化

---
+++ gitclone.js
@@ -0,0 +1,20 @@
+import download from 'download-git-repo'
+import ora from "ora";
+import chalk from "chalk";
+
+const clone = (remote, name, option=false) => {
+  const spinner = ora('正在拉取项目......').start();
+  return new Promise((resolve, reject) => {
+    download(remote, name, option, err =>{
+      if (err) {
+        spinner.fail(chalk.red(err));
+        reject(err)
+        return
+      }
+      spinner.succeed(chalk.green('拉取成功'))
+      resolve();
+    })
+  })
+}
+
+export default clone;
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
+import clone from "./gitclone.js"
+import figlet from 'figlet';
+import chalk from 'chalk';
 
-console.log('hello xt-cli')
+console.log('\r\n' + chalk.greenBright.bold(figlet.textSync('xt-cli', {
+  font: 'Standard',
+  horizontalLayout: 'default',
+  verticalLayout: 'default',
+  width: 80,
+  whitespaceBreak: true
+})));
+console.log(`\r\nRun ${chalk.cyan(`xt-cli <command> --help`)} for detailed usage of given command\r\n`)
+
+await clone("yingside/vue-cli-template", "vue-cli-temp");
